6f107de8acf7f1142cba3c99a66bd1155c313f67
Dag-Roger Eriksen <dag.roger.e@gmail.com>
2025-02-07 15:14:01 +0100

    feat: privacy policy page

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ChallengeView.vue'),
     },
+    {
+      path: '/privacy-policy',
+      name: 'Privacy policy',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/PrivacypolicyView.vue'),
+    },
   ],
 })
 
