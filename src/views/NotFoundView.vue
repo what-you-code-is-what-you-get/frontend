ad71d14794c467367b884893df58603aa71c64a2
Dag-Roger Eriksen <dag.roger.e@gmail.com>
2025-02-09 11:49:23 +0100

    feat: NotFoundView and refactor router

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <router-link to="/">
+      <LargeLogoComponent />
+    </router-link>
+    <h2>404 - Not Found</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LargeLogoComponent from '@/components/LargeLogoComponent.vue'
+</script>
+
+<style scoped>
+.logo-wrapper {
+  padding-top: 200px;
+  padding-bottom: 100px;
+}
+h2 {
+  text-align: center;
+  font-size: var(--step-8);
+  color: var(--color-bv-green);
+}
+</style>
